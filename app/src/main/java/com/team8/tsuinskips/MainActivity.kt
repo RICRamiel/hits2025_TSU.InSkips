5474f666d9a40541b0a26c91ecd5229becb63ba0
RICRamiel <tardis40type@gmail.com>
2025-03-08 22:35:29 +0700

    start

--- app/src/main/java/com/team8/tsuinskips/MainActivity.kt
+++ app/src/main/java/com/team8/tsuinskips/MainActivity.kt
@@ -4,12 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.team8.tsuinskips.ui.theme.TSUInSkipsTheme
 
@@ -19,11 +28,15 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TSUInSkipsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Box(
+                    Modifier
+                        .fillMaxSize()
+                        .paint(
+                            painterResource(R.drawable.login_background),
+                            contentScale = ContentScale.Crop
+                        )
+                ) {
+
                 }
             }
         }
@@ -31,17 +44,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun HelloScreen() {
+    Text(
+        text = R.string.hello.toString(),
+
+    )
+
+}
+
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Hello $name!", modifier = modifier
     )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 100, heightDp = 100)
 @Composable
-fun GreetingPreview() {
-    TSUInSkipsTheme {
-        Greeting("Android")
+fun HelloScreenPreview() {
+    Box(
+        Modifier
+            .fillMaxSize()
+            .paint(
+                painterResource(R.drawable.login_background), contentScale = ContentScale.Crop
+            )
+    ) {
+        HelloScreen()
     }
 }
